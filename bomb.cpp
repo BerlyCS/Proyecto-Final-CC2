5e199f5e377f9d00f0feda4a16ea501930a30921
BerlyCS <berly.mandamientos@gmail.com>
2024-06-26 17:19:01 -0500

    Bomb.cpp

--- bomb.cpp
+++ bomb.cpp
@@ -9,26 +9,26 @@ using namespace sf;
 
 
 
-int main() {
-    RenderWindow window(VideoMode::getFullscreenModes()[0], "Bomberman", Style::Fullscreen);
-    window.setVerticalSyncEnabled(true);
-    const int WIDTH = window.getSize().x;
-    const int HEIGHT = window.getSize().y;
-    Clock clock;
-    Bomba bomb(3,1,300,300, clock.getElapsedTime());
+/* int main() { */
+/*     RenderWindow window(VideoMode::getFullscreenModes()[0], "Bomberman", Style::Fullscreen); */
+/*     window.setVerticalSyncEnabled(true); */
+/*     const int WIDTH = window.getSize().x; */
+/*     const int HEIGHT = window.getSize().y; */
+/*     Clock clock; */
+/*     Bomba bomb(3,1,300,300, clock.getElapsedTime()); */
 
-    while (window.isOpen()) {
-        Event event;
-        //Eventos
-        while (window.pollEvent(event)) {
-            if (event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape))
-                window.close(); 
-        }
+/*     while (window.isOpen()) { */
+/*         Event event; */
+/*         //Eventos */
+/*         while (window.pollEvent(event)) { */
+/*             if (event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape)) */
+/*                 window.close(); */ 
+/*         } */
 
 
-        bomb.draw(window, clock.getElapsedTime());
-        window.display();
-        //logica aqui
-    }
-    return EXIT_SUCCESS;
-}
+/*         bomb.draw(window, clock.getElapsedTime()); */
+/*         window.display(); */
+/*         //logica aqui */
+/*     } */
+/*     return EXIT_SUCCESS; */
+/* } */
