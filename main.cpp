553e110d2c9e58241226b4237eefa565be3cbd13
BerlyCS <berly.mandamientos@gmail.com>
2024-06-28 19:57:12 -0500

    Animacion en player

--- main.cpp
+++ main.cpp
@@ -2,19 +2,18 @@
 #include <SFML/Audio/SoundBuffer.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Rect.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 #include <SFML/Graphics/Texture.hpp>
+#include <SFML/System/Clock.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/VideoMode.hpp>
 #include <SFML/Audio.hpp>
-#include <cmath>
 #include <cstdlib>
-#include <ctime>
-#include <fstream>
 #include <string>
 #include<iostream>
 #include "mapa.cpp"
@@ -62,20 +61,23 @@ class Bomba {
 
 class Player {
     protected:
-        Texture images[4];
+        Texture texture;
+        ASprite down_frames, up_frames, left_frames, right_frames;
         Sprite sprite;
         Vector2f pos;
         double speed;
-        int bombcount;
+        int bombcount, bombpower, lives;
+        bool isAlive;
         RectangleShape hitbox;
     public:
-        Player() {
+        Player() : down_frames(0.2f), up_frames(0.2f), left_frames(0.2f), right_frames(0.2f) {
             speed = 5.0f;
             bombcount = 1;
         }
 
         //para poder cambiar los controles
-        virtual void controlar() = 0;
+        virtual void controlar(float& dt) = 0;
+
         void draw(RenderWindow& win) {
             win.draw(sprite);
         }
@@ -89,36 +91,52 @@ class Player_one : public Player {
             sprite.setPosition(pos);
 
             sprite.setPosition(Vector2f(100,100));
-            if (!(
-                images[0].loadFromFile("images/Character_W_4.png") &&
-                images[1].loadFromFile("images/Character_A_4.png") &&
-                images[2].loadFromFile("images/Character_S_4.png") &&
-                images[3].loadFromFile("images/Character_D_4.png") 
-               )) {
+
+            if (!(texture.loadFromFile("images/IMG_20240628_190401.png"))) {
                 cout<<"No se pudo cargar las texturas del jugador 1"<<endl;
             }
+            sprite.setTexture(texture);
+            sprite.scale(Vector2f(4,4));
+            down_frames.setRects(0, 0, 16, 24, 3);
+            down_frames.addFrame(IntRect(16,0,16,24));
+            up_frames.setRects(0, 24, 16, 24, 3);
+            up_frames.addFrame(IntRect(16,24,16,24));
+            left_frames.setRects(0, 48, 16, 24, 3);
+            left_frames.addFrame(IntRect(16,48,16,24));
+            right_frames.setRects(0, 72, 16, 24, 3);
+            right_frames.addFrame(IntRect(16,72,16,24));
+
+            down_frames.applyToSprite(sprite);
         }
-        void controlar()
+        void controlar(float& dt)
         {
+            down_frames.update(dt);
+            up_frames.update(dt);
+            left_frames.update(dt);
+            right_frames.update(dt);
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
             {
                 sprite.move(0, -speed);
-                sprite.setTexture(images[0]);
+                up_frames.applyToSprite(sprite);
+                /* sprite.setTexture(images[0]); */
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
             {
                 sprite.move(0, speed);
-                sprite.setTexture(images[2]);
+                down_frames.applyToSprite(sprite);
+                /* sprite.setTexture(images[2]); */
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
             {
                 sprite.move(-speed, 0);
-                sprite.setTexture(images[1]);
+                left_frames.applyToSprite(sprite);
+                /* sprite.setTexture(images[1]); */
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
             {
                 sprite.move(speed, 0);
-                sprite.setTexture(images[3]);
+                right_frames.applyToSprite(sprite);
+                /* sprite.setTexture(images[3]); */
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
                 place_bomb(pos);
@@ -126,46 +144,46 @@ class Player_one : public Player {
         }
 };
 
-class Player_two : public Player {
-    public:
-        Player_two() : Player() {
-            sprite.setPosition(Vector2f(100,100));
-            if (!(
-                images[0].loadFromFile("images/W1.png") &&
-                images[1].loadFromFile("images/A1.png") &&
-                images[2].loadFromFile("images/S1.png") &&
-                images[3].loadFromFile("images/D1.png") 
-               )) {
-                cout<<"No se pudo cargar las texturas del jugador 2"<<endl;
-            }
-        }
-        void controlar()
-        {
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-            {
-                sprite.move(0, -speed);
-                sprite.setTexture(images[0]);
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-            {
-                sprite.move(0, speed);
-                sprite.setTexture(images[2]);
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-            {
-                sprite.move(-speed, 0);
-                sprite.setTexture(images[1]);
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-            {
-                sprite.move(speed, 0);
-                sprite.setTexture(images[3]);
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) {
-                /* place_bomb(Vector2i(x,y)); */
-            }
-        }
-};
+/* class Player_two : public Player { */
+/*     public: */
+/*         Player_two() : Player() { */
+/*             sprite.setPosition(Vector2f(100,100)); */
+/*             if (!( */
+/*                 images[0].loadFromFile("images/W1.png") && */
+/*                 images[1].loadFromFile("images/A1.png") && */
+/*                 images[2].loadFromFile("images/S1.png") && */
+/*                 images[3].loadFromFile("images/D1.png") */ 
+/*                )) { */
+/*                 cout<<"No se pudo cargar las texturas del jugador 2"<<endl; */
+/*             } */
+/*         } */
+/*         void controlar() */
+/*         { */
+/*             if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) */
+/*             { */
+/*                 sprite.move(0, -speed); */
+/*                 sprite.setTexture(images[0]); */
+/*             } */
+/*             if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) */
+/*             { */
+/*                 sprite.move(0, speed); */
+/*                 sprite.setTexture(images[2]); */
+/*             } */
+/*             if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) */
+/*             { */
+/*                 sprite.move(-speed, 0); */
+/*                 sprite.setTexture(images[1]); */
+/*             } */
+/*             if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) */
+/*             { */
+/*                 sprite.move(speed, 0); */
+/*                 sprite.setTexture(images[3]); */
+/*             } */
+/*             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) { */
+/*                 /1* place_bomb(Vector2i(x,y)); *1/ */
+/*             } */
+/*         } */
+/* }; */
 
 int main() {
     RenderWindow window(VideoMode::getFullscreenModes()[0], "Bomberman", Style::Fullscreen);
@@ -176,9 +194,10 @@ int main() {
     Menu menu;
     mapa.Print();
     bool Game_started = false;
+    Clock clock;
 
     Player_one player(mapa);
-    Player_two player_dos;
+    /* Player_two player_dos; */
 
     while (window.isOpen()) {
         Event event;
@@ -193,12 +212,13 @@ int main() {
             menu.draw(window);
             continue;
         }
+        float dt = clock.restart().asSeconds();
         window.clear(Color::Black);
-        player.controlar();
-        player_dos.controlar();
+        player.controlar(dt);
+        /* player_dos.controlar(); */
         mapa.draw(window);
         player.draw(window);
-        player_dos.draw(window);
+        /* player_dos.draw(window); */
         window.display();
     }
     return EXIT_SUCCESS;
