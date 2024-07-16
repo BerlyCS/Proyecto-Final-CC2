e8eb80b54ab40e6344c6eaa9c0877b487de5c398
tomaco <autademontag@gmail.com>
2024-07-16 11:41:38 -0500

    new branch

--- main.cpp
+++ main.cpp
@@ -384,8 +384,7 @@ class Player_two : public Player {
 };
 
 int main() {
-    //RenderWindow window(VideoMode::getFullscreenModes()[0], "Bomberman", Style::Fullscreen);
-    sf::RenderWindow window(sf::VideoMode(1000, 1000), "Bomberman");
+    RenderWindow window(VideoMode::getFullscreenModes()[0], "Bomberman", Style::Fullscreen);
     window.setVerticalSyncEnabled(true);
     const int WIDTH = window.getSize().x;
     const int HEIGHT = window.getSize().y;
