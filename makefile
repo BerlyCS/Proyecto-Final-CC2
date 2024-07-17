255acb197e93168bda9b62fb13986164cbd9384e
JairTb <jdtb151@gmail.com>
2024-07-16 23:45:26 -0500

    split mod

--- makefile
+++ makefile
@@ -1,12 +1,15 @@
-main:
-	g++ main.cpp -I/usr/include/ -o test.exe -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -g3 -Wall -Wextra
+main: main.o bomb.o player.o
+	g++ main.o bomb.o player.o -o test.exe -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -g3 -Wall -Wextra
 	./test.exe
 
 windows:
 	g++ main.cpp -I D:\Libs\SFML-2.5.1\include -L D:\libs\SFML-2.5.1\lib -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -o p.exe
 
-bomb: 
-	g++ bomb.cpp -I/usr/include/ -o bomb -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
+bomb.o: bomb.cpp
+	g++ -c bomb.cpp -I/usr/include/ -o bomb.o
 
-powerup: 
+player.o: player.cpp
+	g++ -c player.cpp -I/usr/include/ -o player.o
+
+powerup:
 	g++ powerup.cpp -I/usr/include/ -o powerup -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
--- mapa.cpp
+++ mapa.cpp
@@ -1,260 +1,219 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-#include <SFML/Graphics/Rect.hpp>
-#include <SFML/Graphics/Sprite.hpp>
-#include <SFML/Graphics/Texture.hpp>
-#include <SFML/System/Vector2.hpp>
-#include <cmath>
-#include <queue>
-#include <vector>
-#include <iostream>
-#include <ctime>
-#include <vector>
-#include "animation.cpp"
-#include "bomba.cpp"
-
+#include "mapa.h"
 
 using namespace sf;
 using namespace std;
 
-class Block{
-    protected:
-        Sprite sprite;
-        float sizeBlock;
-    public:
-        Block(int WIDTH, int HEIGHT){
-            int x = WIDTH;
-            int y = HEIGHT;
-            int min = x > y ? y : x;
-            sizeBlock = (float(min) / 13);
-        }
+Block::Block(int WIDTH, int HEIGHT){
+    int x = WIDTH;
+    int y = HEIGHT;
+    int min = x > y ? y : x;
+    sizeBlock = (float(min) / 13);
+}
+
+Sprite& Block::getSprite(){
+    return sprite;
+}
+
+void Block::setSprite(Sprite& sprite) {
+    this->sprite = sprite;
+}
+
+void Block::draw(RenderWindow& window){
+    window.draw(sprite);
+    //window.draw(collider);
+}
+
+float Block::getBlockSize(){
+    return sizeBlock;
+}
 
 
-        Sprite& getSprite(){
-            return sprite;
-        }
+Wall::Wall(int WIDTH, int HEIGHT) : Block(WIDTH, HEIGHT){}
 
-        void setSprite(Sprite& sprite) {
-            this->sprite = sprite;
-        }
+bool Wall::IsCollidable(){
+    return true;
+}
+bool Wall::IsBreakable(){
+    return false;
+}
 
-        void draw(RenderWindow& window){
-            window.draw(sprite);
-            //window.draw(collider);
-        }
 
-        float getBlockSize() {return sizeBlock; }
+WeakWall::WeakWall(int WIDTH, int HEIGHT) : Block(WIDTH, HEIGHT){}
 
-        virtual bool IsCollidable() = 0;
-        virtual bool IsBreakable() = 0;
+bool WeakWall::IsCollidable(){
+    return true;
+}
+bool WeakWall::IsBreakable(){
+    return true;
+}
 
-        virtual ~Block() = default;
-};
 
-class Wall : public Block{
-    public:
-        Wall(int WIDTH, int HEIGHT) : Block(WIDTH, HEIGHT){
-        }
+Tile::Tile(int WIDTH, int HEIGHT) : Block(WIDTH, HEIGHT){}
 
-        bool IsCollidable() {return true;}
-        bool IsBreakable() {return false;}
+bool Tile::IsCollidable(){
+    return false;
+}
+bool Tile::IsBreakable(){
+    return false;
+}
 
-        ~Wall() = default;
-};
 
-class WeakWall : public Block{
-    public:
-        WeakWall(int WIDTH, int HEIGHT) : Block(WIDTH, HEIGHT){
-        }
+Fire_Tile::Fire_Tile(int WIDTH, int HEIGHT) : Block(WIDTH, HEIGHT) {}
 
-        bool IsCollidable() {return true;}
-        bool IsBreakable() {return true;}
+bool Fire_Tile::IsCollidable(){
+    return false;
+}
+bool Fire_Tile::IsBreakable(){
+    return false;
+}
 
-        ~WeakWall() = default;
-};
 
-class Tile : public Block{
-    public:
-        Tile(int WIDTH, int HEIGHT) : Block(WIDTH, HEIGHT){
-        }
+Mapa_2::Mapa_2(int WIDTH, int HEIGHT, int map_style) : map_style(map_style) {
+    screen_size = Vector2i(WIDTH, HEIGHT);
+    texture.loadFromFile("images/wall_textur.png");
+    IntRect frames[3];
 
-        bool IsCollidable() {return false;}
-        bool IsBreakable() {return false;}
-        
-        ~Tile() = default;
-};
+    for(int i =0 ; i<3 ; i++) {
+    frames[2] = IntRect(0,16*map_style,16,16);
+    frames[1] = IntRect(16,16*map_style,16,16);
+    frames[0] = IntRect(32,16*map_style,16,16);
+}
 
-class Fire_Tile : public Block {
-    public:
-        Fire_Tile(int WIDTH, int HEIGHT) : Block(WIDTH, HEIGHT) {
+    int x = WIDTH;
+    int y = HEIGHT;
+    int min = x > y ? y : x;
+    sizeBlock = (float(min) / 13);
+    cout<<"sizeBlock: "<<sizeBlock<<endl;
 
-        }
-
-        bool IsCollidable() {return false;}
-        bool IsBreakable() {return false;
-        }
-        
-};
-
-class Mapa_2 {
-private:
-    Texture texture;
-    float sizeBlock;
-    vector< vector<Block*>> sprites_map;
-    vector< vector<char>> matriz;
-    queue<Bomba*> bombsEvents;
-    Vector2i screen_size;
-    int map_style;
-
-public:
-    Mapa_2(int WIDTH, int HEIGHT, int map_style=0) : map_style(map_style) {
-        screen_size = Vector2i(WIDTH, HEIGHT);
-        texture.loadFromFile("images/wall_textur.png");
-        IntRect frames[3];
-
-        //
-        for(int i =0 ; i<3 ; i++) {
-            frames[2] = IntRect(0,16*map_style,16,16);
-            frames[1] = IntRect(16,16*map_style,16,16);
-            frames[0] = IntRect(32,16*map_style,16,16);
-        }
-
-        int x = WIDTH;
-        int y = HEIGHT;
-        int min = x > y ? y : x;
-        sizeBlock = (float(min) / 13);
-        cout<<"sizeBlock: "<<sizeBlock<<endl;
-
-        matriz = vector<vector<char>>(13, vector<char>(13, ' '));
-        generarMatriz();
-        // Inicializar sprites_map y matriz
-        for (int j = 0; j < 13; j++) {
-            vector<Block*> filaSprites;
-            for (int i = 0; i < 13; i++) {
-                Block* bloque;
-                Sprite sprite;
-                sprite.setTexture(texture);
-                if (matriz[i][j] == '#') {
-                    bloque = new Wall(WIDTH, HEIGHT);
-                    sprite.setTextureRect(frames[0]);
-                } else if (matriz[i][j] == 'x') {
-                    bloque = new WeakWall(WIDTH, HEIGHT);
-                    sprite.setTextureRect(frames[1]);
-                } else {
-                    bloque = new Tile(WIDTH, HEIGHT);
-                    sprite.setTextureRect(frames[2]);
-                }
-                auto size = frames[0].getSize();
-                bloque->setSprite(sprite);
-                bloque->getSprite().setScale(sizeBlock/size.x, sizeBlock/size.y);
-                bloque->getSprite().setPosition(sizeBlock * j, sizeBlock * i);
-                filaSprites.push_back(bloque);
+    matriz = vector<vector<char>>(13, vector<char>(13, ' '));
+    generarMatriz();
+    // Inicializar sprites_map y matriz
+    for (int j = 0; j < 13; j++) {
+        vector<Block*> filaSprites;
+        for (int i = 0; i < 13; i++) {
+            Block* bloque;
+            Sprite sprite;
+            sprite.setTexture(texture);
+            if (matriz[i][j] == '#') {
+                bloque = new Wall(WIDTH, HEIGHT);
+                sprite.setTextureRect(frames[0]);
+            } else if (matriz[i][j] == 'x') {
+                bloque = new WeakWall(WIDTH, HEIGHT);
+                sprite.setTextureRect(frames[1]);
+            } else {
+                bloque = new Tile(WIDTH, HEIGHT);
+                sprite.setTextureRect(frames[2]);
             }
-            sprites_map.push_back(filaSprites);
+            auto size = frames[0].getSize();
+            bloque->setSprite(sprite);
+            bloque->getSprite().setScale(sizeBlock/size.x, sizeBlock/size.y);
+            bloque->getSprite().setPosition(sizeBlock * j, sizeBlock * i);
+            filaSprites.push_back(bloque);
         }
+        sprites_map.push_back(filaSprites);
     }
+}
 
-    void to_tile_at(Vector2i index) {
-        /* cout<<flush<<"Deleting: "<<index.x<<' '<<index.y<<' '<<sprites_map[index.x][index.y]<<endl; */
-        /* cout<<flush<<"deleting..."<<endl; */
-        /* delete sprites_map[index.y][index.x]; */
-        /* cout<<flush<<"deleted!"<<endl; */
-        /* cout<<flush<<"setting new tile..."<<endl; */
-        sprites_map[index.x][index.y] = new Tile(get_screen_size().x, get_screen_size().y);
-        Block* bloque = sprites_map[index.x][index.y];
+void Mapa_2::to_tile_at(Vector2i index) {
+    /* cout<<flush<<"Deleting: "<<index.x<<' '<<index.y<<' '<<sprites_map[index.x][index.y]<<endl; */
+    /* cout<<flush<<"deleting..."<<endl; */
+    /* delete sprites_map[index.y][index.x]; */
+    /* cout<<flush<<"deleted!"<<endl; */
+    /* cout<<flush<<"setting new tile..."<<endl; */
+    sprites_map[index.x][index.y] = new Tile(get_screen_size().x, get_screen_size().y);
+    Block* bloque = sprites_map[index.x][index.y];
 
-        /* cout<<flush<<"done..."<<endl; */
+    /* cout<<flush<<"done..."<<endl; */
 
-        Sprite sprite;
-        sprite.setTexture(texture);
-        IntRect frames = IntRect(0,16*map_style,16,16);
-        sprite.setTextureRect(frames);
+    Sprite sprite;
+    sprite.setTexture(texture);
+    IntRect frames = IntRect(0,16*map_style,16,16);
+    sprite.setTextureRect(frames);
 
-        auto size = frames.getSize();
-        bloque->setSprite(sprite);
-        bloque->getSprite().setScale(sizeBlock/size.x, sizeBlock/size.y);
-        bloque->getSprite().setPosition(sizeBlock * index.x, sizeBlock * index.y);
-    }
+    auto size = frames.getSize();
+    bloque->setSprite(sprite);
+    bloque->getSprite().setScale(sizeBlock/size.x, sizeBlock/size.y);
+    bloque->getSprite().setPosition(sizeBlock * index.x, sizeBlock * index.y);
+}
 
-    Vector2i get_screen_size() const {
-        return screen_size;
-    }
+Vector2i Mapa_2::get_screen_size() const {
+    return screen_size;
+}
 
-    /* Toma una coordenada del mapa y retorna la posicion en pantalla*/
-    Vector2f get_coords(Vector2i pos) const {
-        /* cout<<x*sizeBlock<<' '<<y*sizeBlock<<endl; */
-        return Vector2f(pos.x*sizeBlock,pos.y*sizeBlock);
-    }
+/* Toma una coordenada del mapa y retorna la posicion en pantalla*/
+Vector2f Mapa_2::get_coords(Vector2i pos) const {
+    /* cout<<x*sizeBlock<<' '<<y*sizeBlock<<endl; */
+    return Vector2f(pos.x*sizeBlock,pos.y*sizeBlock);
+}
 
-    /* Vector2i get_coords(Vector2f pos) { */
-    /*     return Vector2i(pos.x/sizeBlock, pos.y/sizeBlock); */
-    /* } */
+/* Vector2i get_coords(Vector2f pos) { */
+/*     return Vector2i(pos.x/sizeBlock, pos.y/sizeBlock); */
+/* } */
 
-    /*Toma una coordenada de pantalla y retorna la posicion en la matriz*/
-    Vector2i get_mat_coords(Vector2f pos) {
-        return Vector2i((pos.x/sizeBlock), (pos.y/sizeBlock));
-    }
+/*Toma una coordenada de pantalla y retorna la posicion en la matriz*/
+Vector2i Mapa_2::get_mat_coords(Vector2f pos) {
+    return Vector2i((pos.x/sizeBlock), (pos.y/sizeBlock));
+}
 
-    Block*& get_block_at(int x, int y) {
-        return sprites_map[x][y];
-    }
+Block*& Mapa_2::get_block_at(int x, int y) {
+    return sprites_map[x][y];
+}
 
-    int get_map_style() {
-        return map_style;
-    }
+int Mapa_2::get_map_style() {
+    return map_style;
+}
 
-    void generarMatriz() {
-        srand(time(NULL));
-        for (int j = 0; j < 13; j++) {
-            for (int k = 0; k < 13; k++) {
-                if (j == 0 || k == 0 || j == 12 || k == 12) {
-                    matriz[j][k] = '#';  // Asignar '#' a la matriz en los bordes
+void Mapa_2::generarMatriz() {
+    srand(time(NULL));
+    for (int j = 0; j < 13; j++) {
+        for (int k = 0; k < 13; k++) {
+            if (j == 0 || k == 0 || j == 12 || k == 12) {
+                matriz[j][k] = '#';  // Asignar '#' a la matriz en los bordes
+            } else {
+                if (j % 2 == 0 && k % 2 == 0) {
+                    matriz[j][k] = '#';  // Asignar '#' a la matriz en posiciones pares
                 } else {
-                    if (j % 2 == 0 && k % 2 == 0) {
-                        matriz[j][k] = '#';  // Asignar '#' a la matriz en posiciones pares
+                    if((j<=2 && k<=2) || (j>=10 && k>=10)){
+                        matriz[j][k] = ' ';
+                        continue;
+                    }
+                    int aux = rand() % 6;
+                    if (aux == 0) {
+                        matriz[j][k] = ' ';
                     } else {
-                        if((j<=2 && k<=2) || (j>=10 && k>=10)){
-                            matriz[j][k] = ' ';
-                            continue;
-                        }
-                        int aux = rand() % 6;
-                        if (aux == 0) {
-                            matriz[j][k] = ' ';
-                        } else {
-                            matriz[j][k] = 'x';
-                        }
+                        matriz[j][k] = 'x';
                     }
                 }
             }
         }
     }
+}
 
-    void Print() const {
-        for (int i = 0; i < 13; i++) {
-            for (int j = 0; j < 13; j++) {
-                std::cout << matriz[i][j] << ' ';
-            }
-            std::cout << std::endl;
+void Mapa_2::Print() const {
+    for (int i = 0; i < 13; i++) {
+        for (int j = 0; j < 13; j++) {
+            std::cout << matriz[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
+
+void Mapa_2::draw(RenderWindow& window) {
+    for (int i = 0; i < 13; i++) {
+        for (int j = 0; j < 13; j++) {
+            sprites_map[i][j]->draw(window);
         }
     }
+}
 
-    void draw(RenderWindow& window) {
-        for (int i = 0; i < 13; i++) {
-            for (int j = 0; j < 13; j++) {
-                sprites_map[i][j]->draw(window);
-            }
-        }
-    }
+vector<vector<char>> Mapa_2::getMatriz(){
+    return matriz;
+}
 
-    vector<vector<char>> getMatriz(){
-        return matriz;
-    }
+vector<vector<Block*>> Mapa_2::getMatrizSprites(){
+    return sprites_map;
+}
 
-    vector<vector<Block*>> getMatrizSprites(){
-        return sprites_map;
-    }
-
-    int getBlockSize() {
-        return sizeBlock;
-    }
-};
+int Mapa_2::getBlockSize() {
+    return sizeBlock;
+}
