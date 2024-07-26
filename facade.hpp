1201d135e7c52d84ead4e0699109765ea9aeed45
Jair <jdtb151@gmail.com>
2024-07-26 08:45:14 -0500

    header

--- facade.hpp
+++ facade.hpp
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include "player.h"
 #include "menu.h"
+#include <SFML/Graphics/Font.hpp>
 #include <chrono>
 #include <thread>
 #include "sound.hpp"
@@ -12,6 +13,7 @@ using namespace sf;
 class Facade_game {
     private:
         RenderWindow window;
+        Font font;
         Mapa_2 mapa;
         Menu menu;
         bool Game_started;
